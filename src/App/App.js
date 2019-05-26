c8c2f8e871fe07b85e56d4e17493dec3f6e99ac3
free4mOriginal <free4m@gmail.com>
2019-05-26 10:20:33 -0500

    Reflow button design

--- src/App/App.js
+++ src/App/App.js
@@ -25,23 +25,22 @@ class App extends Component {
             <h2>About Me</h2>
             <div id="aboutBox">
               <p>I received my formal training from the Erevan Art Academy, Armenia in Fine Art and Ceramics and worked and exhibited in the US, Europe and the Middle East. My work is a unique blend of organic design and sculptural jewelry, using both precious and semi-precious stones hand-picked for quality clarity and uniqueness.</p>
-
-              <div id="filter">
-                <button type="Handmade">Handmade Collection</button>
-                <button type="Traditional">Traditional Custom Jewelry</button>
-                <button type="ZBrush">ZBrush Modeling</button>
-              </div>
-              <p></p>
             </div>
 
             <div id="contactBox">
               <ul>
-                <li><h3>Contact</h3></li>
                 <li>5 1 0 . 3 3 3 . 5 4 4 0</li>
                 <li>KarinaLiner @ gmail</li>
-                <li id="social"><p><a href="https://www.linkedin.com/in/karina-liner-5530a03a/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a> . <a href="https://www.instagram.com/karinaliner/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a> . <a href="https://www.instagram.com/karinaliner/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a></p>
+                <li id="social">
+                  <a href="https://www.linkedin.com/in/karina-liner-5530a03a/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a> . <a href="https://www.instagram.com/karinaliner/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a> . <a href="https://www.instagram.com/karinaliner/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
                 </li>
               </ul>
+            </div>
+
+            <div id="filter">
+              <button type="Handmade">Handmade Collection</button>
+              <button type="Traditional">Traditional Custom Jewelry</button>
+              <button type="ZBrush">ZBrush Modeling</button>
             </div>
           </div>
         </aside>
