fe3938b7d134a5ea12ac0cf7b41dc2b24f50a6a2
free4mOriginal <free4m@gmail.com>
2019-06-02 18:58:23 -0500

    Remove /assets, rearrange /images, add info to GPhotos.js

--- src/util/FeedStaticObj.js
+++ src/util/FeedStaticObj.js
@@ -2,129 +2,129 @@ const imagesALL = {
     Handmade: [
         {
             name: "biruza",
-            src: `${require('../assets/images/handmade/biruza-big.png')}`,
+            src: `${require('../images/handmade/biruza-big.png')}`,
             description: "Square tourquiose ring, sterling silver",
         },
     
         {
             name: "dot",
-            src: `${require('../assets/images/handmade/dot-neckalce-small.png')}`,
+            src: `${require('../images/handmade/dot-neckalce-small.png')}`,
             description: "Handmade sterling silver necklace",
         },
     
         {
             name: "garnetNecklace",
-            src: `${require('../assets/images/handmade/garnet-necklace-small.png')}`,
+            src: `${require('../images/handmade/garnet-necklace-small.png')}`,
             description: "Bright garnet necklace, sterling silver",
         },
     
         {
             name: "pearl",
-            src: `${require('../assets/images/handmade/pearl-upright.png')}`,
+            src: `${require('../images/handmade/pearl-upright.png')}`,
             description: "Inlaid 14K gold pearl ring, sterling silver",
         },
     
         {
             name: "square",
-            src: `${require('../assets/images/handmade/square-tourmaline-small.png')}`,
+            src: `${require('../images/handmade/square-tourmaline-small.png')}`,
             description: "Square tourmaline ring, sterling silver",
         },
 
         {
             name: "bullet",
-            src: `${require('../assets/images/handmade/bullet-big.png')}`,
+            src: `${require('../images/handmade/bullet-big.png')}`,
             description: "Bullet garnet, sterling silver",
         },
 
         {
             name: "bulletSquare",
-            src: `${require('../assets/images/handmade/bullet-square-small.png')}`,
+            src: `${require('../images/handmade/bullet-square-small.png')}`,
             description: "Bullet square ring, garnet, sterling silver",
         },
 
         {
             name: "lavaHandmade",
-            src: `${require('../assets/images/handmade/lava-side.png')}`,
+            src: `${require('../images/handmade/lava-side.png')}`,
             description: "Lava bead set in sterling silver",
         },
 
         {
             name: "signature",
-            src: `${require('../assets/images/handmade/signature-ring-upright.png')}`,
+            src: `${require('../images/handmade/signature-ring-upright.png')}`,
             description: "Signature ring, sterling silver",
         },
     ],
     Traditional: [
         {
             name: "leaf",
-            src: `${require('../assets/images/traditional/1122.jpg')}`,
+            src: `${require('../images/traditional/1122.jpg')}`,
             description: "Leaf design, 14K gold engagement & band",
         },
     
         {
             name: "three",
-            src: `${require('../assets/images/traditional/1133.jpg')}`,
+            src: `${require('../images/traditional/1133.jpg')}`,
             description: "Three-stone center, 14K gold Eng ring",
         },
     
         {
             name: "fingerprint",
-            src: `${require('../assets/images/traditional/fingerprint-ring.jpg')}`,
+            src: `${require('../images/traditional/fingerprint-ring.jpg')}`,
             description: "Keepsake fingerprint ring",
         },
     
         {
             name: "antique",
-            src: `${require('../assets/images/traditional/antique.jpg')}`,
+            src: `${require('../images/traditional/antique.jpg')}`,
             description: "Antique-style 14KY gold engagement ring",
         },
 
         {
             name: "round",
-            src: `${require('../assets/images/traditional/1097.jpg')}`,
+            src: `${require('../images/traditional/1097.jpg')}`,
             description: "Rounded 14KY gold engagement ring",
         },
 
         {
             name: "rose",
-            src: `${require('../assets/images/traditional/1117.jpg')}`,
+            src: `${require('../images/traditional/1117.jpg')}`,
             description: "Stylized Rose 14K gold engagement ring",
         },
     ],
     ZBrush: [
         {
             name: "pet",
-            src: `${require('../assets/images/zbrush/gosha.jpg')}`,
+            src: `${require('../images/zbrush/gosha.jpg')}`,
             description: "Pet ring, 3D model",
         },
     
         {
             name: "lava",
-            src: `${require('../assets/images/zbrush/lava.jpg')}`,
+            src: `${require('../images/zbrush/lava.jpg')}`,
             description: "Decorative Lava Bead ring, 3D model",
         },
     
         {
             name: "lev",
-            src: `${require('../assets/images/zbrush/lev.jpg')}`,
+            src: `${require('../images/zbrush/lev.jpg')}`,
             description: "Signet ring with Lion Head, 3D model",
         },
 
         {
             name: "byk",
-            src: `${require('../assets/images/zbrush/byk.jpg')}`,
+            src: `${require('../images/zbrush/byk.jpg')}`,
             description: "Taurus Zodiak ring, 3D model",
         },
 
         {
             name: "lizardNecklace",
-            src: `${require('../assets/images/zbrush/lizard.jpg')}`,
+            src: `${require('../images/zbrush/lizard.jpg')}`,
             description: "Lizard necklace with diamonds, 3D model",
         },
 
         {
             name: "lizardRing",
-            src: `${require('../assets/images/zbrush/lizard-ring.jpg')}`,
+            src: `${require('../images/zbrush/lizard-ring.jpg')}`,
             description: "Lizard ring with diamond, 3D model",
         },
     ],
--- src/util/GPhotos.js
+++ src/util/GPhotos.js
@@ -1,4 +1,15 @@
+import axios from 'axios';
+
 // Client ID:
 // 1093732015204-1futk3s07aocb3oq7ltoreul0gnrvg4k.apps.googleusercontent.com
 // Client Secret:
 // GbKjFmMS6mPgZlyeSBZXRBtV
+
+// Shared album link:
+// https://photos.app.goo.gl/NMKSCGq7w9M8heRk8
+
+// HTTP GET request for specific album:
+// https://photoslibrary.googleapis.com/v1/albums/{albumId}
+
+// Album ID:
+// NMKSCGq7w9M8heRk8
