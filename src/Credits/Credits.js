80344d72b98d68d8b4877ea07e838a38fe9b06da
Zhana Liner <45800952+free4mOriginal@users.noreply.github.com>
2023-12-08 18:50:11 -0600

    Update Credits

--- src/Credits/Credits.js
+++ src/Credits/Credits.js
@@ -7,7 +7,7 @@ class Credits extends React.Component {
         return (
             <div className="credits">
                 <p><a href="#top">Back to top</a></p>
-                <span>&copy; 2019 <span className="white">Karina Liner</span> – Jewelry, Photography</span>
+                <span>&copy; 2024
                 <span><span className="white">Zhana Liner</span> – Design & Web Development</span>
                 <span className="smaller">Brian Patrick Tagalog, wallpaper</span>
             </div>
